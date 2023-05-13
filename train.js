ab87a83c7fa7f360d5ea48dd2522700afe6c2a36
Marvelous Osakpolor <efebeosas@gmail.com>
2023-05-13 23:08:30 +0100

    Worked on Object

--- train.js
+++ train.js
@@ -1,10 +1,30 @@
 "use strict"
 
 const Person = {
-    Name: 'Favour Osakpolor Enofe',
-    Age: 12,
+    Name: {
+        first: "Favour" ,
+        middle: "Osakpolor", 
+        last: "Enofe"
+    },
+    Age : 12,
     favouriteColour : 'Red',
     food : 'Rice',
+    location : {
+        streetNumber : 5,
+        street : "Grace Avenue, Estate 10",
+        nearestBusStop : "Off Ring Road",
+        lga : "Obafemi Owode Local Government Area",
+        city : "Redemption camp, Mowe",
+        state : "Ogun State",
+        zipCode: 110111,
+        country : "Nigeria",
+        Continent : "Africa"
+    },
+    getAddress() {
+        return `${this.Name.first} ${this.Name.middle} ${this.Name.last} stays at ${this.location.streetNumber} ${this.location.street}, ${this.location.nearestBusStop}, ${this.location.lga}, ${this.location.city}, ${this.location.state}, ${this.location.zipCode} ${this.location.country} ${this.location.Continent}`
+    }
 }
 
-console.log(Person.Name)
+console.log(Person.getAddress())
+// console.log(Person.Name)
+// console.log(Person.favouriteColour)
