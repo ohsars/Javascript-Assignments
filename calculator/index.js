e316d9afeaad3932e52e69d1aab67e50fdc2832d
Marvelous Osakpolor <efebeosas@gmail.com>
2023-05-14 22:21:53 +0100

    made few changes to calculator

--- calculator/index.js
+++ calculator/index.js
@@ -1,2 +1,33 @@
 "use strict"
 
+// assigning the buttons to its number and making it an integer
+
+const numDisplay = document.getElementById("numberDisplay").innerHTML
+console.log(numDisplay)
+
+
+
+const one = parseInt(document.getElementById("one").innerHTML);
+const two = parseInt(document.getElementById("two").innerHTML);
+const three = parseInt(document.getElementById("three").innerHTML);
+const four = parseInt(document.getElementById("four").innerHTML);
+const five = parseInt(document.getElementById("five").innerHTML);
+const six = parseInt(document.getElementById("six").innerHTML);
+const seven = parseInt(document.getElementById("seven").innerHTML);
+const eight = parseInt(document.getElementById("eight").innerHTML);
+const nine = parseInt(document.getElementById("nine").innerHTML);
+const zero = parseInt(document.getElementById("zero").innerHTML);
+console.log(seven)
+
+
+
+
+
+
+
+
+
+// create a function for division, addition, multiplication and subtraction
+
+
+// console.log(division)
