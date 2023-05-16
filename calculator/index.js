4cbbba070b40f2c2630ee91aab16a14fd6203656
Marvelous Osakpolor <efebeosas@gmail.com>
2023-05-16 09:01:26 +0100

    Added few functionalities

--- calculator/index.js
+++ calculator/index.js
@@ -1,33 +1,51 @@
 "use strict"
+let screen = "0"
+const display = document.querySelector(".calculatorDisplay");
 
-// assigning the buttons to its number and making it an integer
+function buttonClick(value) {
+    isNaN(parseInt(value)) ? handleSymbol(value) : handleNumber(value);
+    rerender();
+}
 
-const numDisplay = document.getElementById("numberDisplay").innerHTML
-console.log(numDisplay)
+function handleNumber(number) {
+    (screen === '0') ? (screen = number) : (screen += number);
 
+}
 
+function handleSymbol(symbol) {
+    switch (symbol) {
+        case "🗑️" :
+            screen = "0";
+            break
+        case "✔️" :
+            console.log('equals');
+            break
+        case "←" : 
+            (screen.length === 1 ? screen = "0" : screen = screen.substring(0, screen.length - 1));
+            break
+        case "➕":
+            console.log("Addition");
+            break
+        case "➖":
+            console.log("minus");
+            break
+        case "❌":
+            console.log("multiply");
+            break
+        case "➗":
+            console.log("divide");
+    }
+}
 
-const one = parseInt(document.getElementById("one").innerHTML);
-const two = parseInt(document.getElementById("two").innerHTML);
-const three = parseInt(document.getElementById("three").innerHTML);
-const four = parseInt(document.getElementById("four").innerHTML);
-const five = parseInt(document.getElementById("five").innerHTML);
-const six = parseInt(document.getElementById("six").innerHTML);
-const seven = parseInt(document.getElementById("seven").innerHTML);
-const eight = parseInt(document.getElementById("eight").innerHTML);
-const nine = parseInt(document.getElementById("nine").innerHTML);
-const zero = parseInt(document.getElementById("zero").innerHTML);
-console.log(seven)
+function init() {
+    console.log("first");
+    document.querySelector(".calculatorkeys").addEventListener("click", function(event) {
+        buttonClick(event.target.innerText)
+    })
+} 
 
+function rerender() {
+    display.innerText = screen
+}
 
-
-
-
-
-
-
-
-// create a function for division, addition, multiplication and subtraction
-
-
-// console.log(division)
+init() 
