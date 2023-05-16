687416ac88874bc59b85f6a729f6b441c9e796d1
Marvelous Osakpolor <efebeosas@gmail.com>
2023-05-16 09:57:04 +0100

    Mathematical Operations now sorted

--- calculator/index.js
+++ calculator/index.js
@@ -1,51 +1,84 @@
-"use strict"
-let screen = "0"
+"use strict";
+let screen = "0";
+let previousOperator = "";
+let runningTotal = 0;
 const display = document.querySelector(".calculatorDisplay");
 
 function buttonClick(value) {
-    isNaN(parseInt(value)) ? handleSymbol(value) : handleNumber(value);
-    rerender();
+  isNaN(parseInt(value)) ? handleSymbol(value) : handleNumber(value);
+  rerender();
 }
 
 function handleNumber(number) {
-    (screen === '0') ? (screen = number) : (screen += number);
-
+  screen === "0" ? (screen = number) : (screen += number);
 }
 
+function handleMath (value) {
+  if (screen === "0") {
+    // do nothing
+    return;
+  }
+
+  const intScreen = parseInt(screen);
+  (runningTotal === 0) ? (runningTotal = intScreen) : flushOperation(intScreen);
+
+  previousOperator = value;
+  screen = "0";
+  console.log(runningTotal);
+};
+
+function flushOperation (intScreen) {
+  if (previousOperator === "➕") {
+    runningTotal += intScreen;
+  } else if (previousOperator === "➖") {
+    runningTotal -= intScreen;
+  } else if (previousOperator === "❌") {
+    runningTotal *= intScreen;
+  } else if (previousOperator === "➗") {
+    runningTotal /= intScreen;
+  }
+};
+
 function handleSymbol(symbol) {
-    switch (symbol) {
-        case "🗑️" :
-            screen = "0";
-            break
-        case "✔️" :
-            console.log('equals');
-            break
-        case "←" : 
-            (screen.length === 1 ? screen = "0" : screen = screen.substring(0, screen.length - 1));
-            break
-        case "➕":
-            console.log("Addition");
-            break
-        case "➖":
-            console.log("minus");
-            break
-        case "❌":
-            console.log("multiply");
-            break
-        case "➗":
-            console.log("divide");
-    }
+  switch (symbol) {
+    case "🗑️":
+      screen = "0";
+      break;
+    case "✔️":
+      if (previousOperator === null ) {
+        // do nothing
+        return;
+      }
+      flushOperation(parseInt(screen));
+      previousOperator = "";
+      screen = "" + runningTotal;
+      runningTotal = 0;
+
+      break;
+    case "←":
+      screen.length === 1
+        ? (screen = "0")
+        : (screen = screen.substring(0, screen.length - 1));
+      break;
+    case "➕":
+    case "➖":
+    case "❌":
+    case "➗":
+      handleMath(symbol);
+      break
+  }
 }
 
 function init() {
-    console.log("first");
-    document.querySelector(".calculatorkeys").addEventListener("click", function(event) {
-        buttonClick(event.target.innerText)
-    })
-} 
-
-function rerender() {
-    display.innerText = screen
+  document
+    .querySelector(".calculatorkeys")
+    .addEventListener("click", function (event) {
+      buttonClick(event.target.innerText);
+    });
 }
 
-init() 
+function rerender() {
+  display.innerText = screen;
+}
+
+init();
